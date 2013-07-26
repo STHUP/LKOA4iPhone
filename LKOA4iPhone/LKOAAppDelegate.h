cbb41cb62bf7a979e097954db678aa8e48b86954
suntinghui <tinghuisun@163.com>
2013-07-26 22:02:05 +0800

    Initial Commit

---
+++ LKOA4iPhone/LKOAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKOAAppDelegate.h
+//  LKOA4iPhone
+//
+//  Created by  STH on 7/26/13.
+//  Copyright (c) 2013 DHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKOAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LKOA4iPhone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LKOA4iPhone
+//
+//  Created by  STH on 7/26/13.
+//  Copyright (c) 2013 DHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LKOAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LKOAAppDelegate class]));
+    }
+}
