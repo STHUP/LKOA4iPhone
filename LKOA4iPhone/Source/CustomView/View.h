690f836463b9bdee0326f5002a21e94ee6715479
suntinghui <tinghuisun@163.com>
2013-07-27 16:26:53 +0800

    STH

---
+++ LKOA4iPhone/Source/CustomView/View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  LKOA4iPhone
+//
+//  Created by  STH on 7/27/13.
+//  Copyright (c) 2013 DHC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface View : NSObject
+
+@end
---
+++ LKOA4iPhone/Source/CustomView/View.m
@@ -0,0 +1,13 @@
+//
+//  View.m
+//  LKOA4iPhone
+//
+//  Created by  STH on 7/27/13.
+//  Copyright (c) 2013 DHC. All rights reserved.
+//
+
+#import "View.h"
+
+@implementation View
+
+@end
