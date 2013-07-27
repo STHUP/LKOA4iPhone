82cde865c237bb7ea1177cf22f6afd2c843572f4
liaojia <liaojia19880128@163.com>
2013-07-27 14:06:58 +0800

    23

---
+++ LKOA4iPhone/Source/ViewController/BaseViewController.h
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.h
+//  LKOA4iPhone
+//
+//  Created by liao jia on 13-7-27.
+//  Copyright (c) 2013年 DHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController<UITextFieldDelegate>
+
+@property(assign, nonatomic)BOOL hasNav;
+
+@end
